b149b4260d0afe68281a4b638a5a30f09e51a830
Saif Uddin <admin@saifuddin.uk>
2024-07-15 19:06:58 +0100

    tidied JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,14 @@
 const loginModal = document.querySelector("#login-modal");
 
 document.addEventListener('click', (event)=>{
-    if (event.target.matches('#login-button')) {
+    if (event.target.matches('#login-button') || 
+        event.target.matches('.close-icon')) {
         loginModal.classList.toggle('hide-modal')
     }
 })
 
 document.addEventListener('keydown', (event)=>{
     if (event.key === "Escape" && loginModal.className != 'hide-modal') {
-        loginModal.classList.toggle('hide-modal')
-    }
-})
-
-document.addEventListener('click', (event)=>{
-    if (event.target.matches('.close-icon')) {
         loginModal.classList.toggle('hide-modal')
     }
 })
