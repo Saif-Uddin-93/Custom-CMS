929f0732d87367ea9366e69619842b5cb4bf85ea
Saif Uddin <admin@saifuddin.uk>
2024-07-20 11:08:53 +0100

    added userdb in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@ const loginModal = document.querySelector("#login-modal");
 const loginBtn = document.querySelector('#login-button');
 const user = document.querySelector('#login-user-input');
 const pass = document.querySelector('#login-password-input');
-const username = 'saif';
-const password = 'pass';
+// const username = users[0].username;
+// const password = users[0].password;
 const signInBtn = document.querySelector('#sign-in-button')
 
 document.addEventListener('click', (event)=>{
@@ -15,10 +15,31 @@ document.addEventListener('click', (event)=>{
 
     // check user login
     if (event.target.matches('#sign-in-button') && event.target.textContent === 'Log in'){
-        if (String(user.value).toLowerCase() === username && pass.value === password){
-            console.log('login successful');
-            location.href = './userprofile.html';
-        } else {console.log('login unsuccessful')}
+        const localObj = Object.entries(localStorage);
+        if (String(user.value).toLowerCase() in localObj){
+            const username = String(user.value).toLowerCase();
+            if (pass.value === localObj[username]){
+                console.log('login successful');
+                location.href = './userprofile.html';
+            } else {console.log('password incorrect')}
+        }
+        else {
+            console.log("user does not exist")
+        }
+    }
+
+    // add new user
+    if (event.target.matches('#sign-in-button') && event.target.textContent === 'Sign up'){
+        const reTypePass = document.querySelector("#login-retype-password-input")
+        if (pass.value === reTypePass.value){
+            const userLogin = {
+                username : user.value,
+                password : pass.value
+            }
+            saveLocal(userLogin);
+            console.log('user created');
+            location.href = `./userprofile.html#${userLogin.username}`;
+        } else {console.log('passwords do not match')}
     }
 
     if (event.target.matches('#log-out')){
@@ -37,3 +58,9 @@ document.addEventListener('keydown', (event)=>{
         loginModal.classList.toggle('hide-modal')
     }
 })
+
+function saveLocal(user){
+    const users = JSON.parse(localStorage.getItem("users")) || []
+    users.push(user)
+    localStorage.setItem("users", JSON.stringify(users))
+}
