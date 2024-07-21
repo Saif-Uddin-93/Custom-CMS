a510c8b985b0fc9a653008eba9872794c57d317a
Saif Uddin <admin@saifuddin.uk>
2024-07-21 11:52:55 +0100

    tidied code and added event listener for login submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,86 +4,66 @@ const userEl = document.querySelector('#login-user-input');
 const passEl = document.querySelector('#login-password-input');
 const signInBtn = document.querySelector('#sign-in-button')
 
-document.addEventListener('click', (event)=>{
-    // // open/close login modal
-    // if (event.target.matches('#login-button') ||
-    //     event.target.matches('.close-icon')) {
-    //     loginModal.classList.toggle('hide-modal');
-    // }
+function checkUserLogin(){
+    const user = String(userEl.value).toLowerCase();
+    const pass = String(passEl.value);
+    const submittedLogin = JSON.parse(localStorage.getItem("CMS_users")) 
+    ? JSON.parse(localStorage.getItem("CMS_users")).map((u)=>{
+        // console.log(Object.values(u).includes(user), u, Object.values(u))
+        return u.username === user ? u : undefined
+    }).filter((u)=>{
+        //console.log(u)
+        return u !== undefined
+    }) : []
 
-    // check user login
-    if (event.target.matches('#sign-in-button') && event.target.textContent === 'Log in'){
-        const user = String(userEl.value).toLowerCase();
-        const pass = String(passEl.value);
-        // console.log("user:", user)
-        // console.log("pass:", pass)
+    // console.log(submittedLogin)
+
+    if (submittedLogin[0]) {
+        const password = submittedLogin[0].password;
+        if (pass === password){
+            saveLocal(submittedLogin[0], false)
+            location.href = `./userprofile.html#${user}`;
+            console.log('login successful');
+        } else {console.log('password incorrect')}
+    } else {
+        console.log(`${user} does not exist`)
+    }
+}
+
+function addNewUser(){
+    const user = String(userEl.value).toLowerCase();
+    const pass = String(passEl.value);
+    const reTypePass = String(document.querySelector("#login-retype-password-input").value)
+    if (user){
         const submittedLogin = JSON.parse(localStorage.getItem("CMS_users")) 
         ? JSON.parse(localStorage.getItem("CMS_users")).map((u)=>{
-            // console.log(Object.values(u).includes(user), u, Object.values(u))
+            // console.log(Object.values(u).includes(user), user, Object.values(u))
             return u.username === user ? u : undefined
         }).filter((u)=>{
-            //console.log(u)
             return u !== undefined
         }) : []
+        if (!submittedLogin[0]) {
+            if (pass === reTypePass){
+                const userLogin = {
+                    username : user,
+                    password : pass
+                }
+                saveLocal(userLogin, true);
+                console.log(`user: ${user} created`);
+                location.href = `./userprofile.html#${userLogin.username}`;
+            } else {console.log('passwords do not match')}
+        } else {console.log(`user: ${user} already exists`)}
+    } else {console.log('enter a username')}
+}
 
-        // console.log(submittedLogin)
+function logOut(){
+    saveLocal({username:''}, false)
+    location.href = './login.html'
+}
 
-        if (submittedLogin[0]) {
-            const password = submittedLogin[0].password;
-            if (pass === password){
-                saveLocal(submittedLogin[0], false)
-                location.href = `./userprofile.html#${user}`;
-                console.log('login successful');
-            } else {console.log('password incorrect')}
-        } else {
-            console.log(`${user} does not exist`)
-        }
-    }
-
-    // add new user
-    if (event.target.matches('#sign-in-button') && event.target.textContent === 'Sign up'){
-        const user = String(userEl.value).toLowerCase();
-        const pass = String(passEl.value);
-        const reTypePass = String(document.querySelector("#login-retype-password-input").value)
-        if (user){
-            const submittedLogin = JSON.parse(localStorage.getItem("CMS_users")) 
-            ? JSON.parse(localStorage.getItem("CMS_users")).map((u)=>{
-                // console.log(Object.values(u).includes(user), user, Object.values(u))
-                return u.username === user ? u : undefined
-            }).filter((u)=>{
-                return u !== undefined
-            }) : []
-            if (!submittedLogin[0]) {
-                if (pass === reTypePass){
-                    const userLogin = {
-                        username : user,
-                        password : pass
-                    }
-                    saveLocal(userLogin, true);
-                    console.log(`user: ${user} created`);
-                    location.href = `./userprofile.html#${userLogin.username}`;
-                } else {console.log('passwords do not match')}
-            } else {console.log(`user: ${user} already exists`)}
-        } else {console.log('enter a username')}
-    }
-
-    if (event.target.matches('#log-out')){
-        saveLocal({username:''}, false)
-        location.href = './login.html'
-    }
-
-    // create new user
-    if (event.target.matches('#new-user-button')){
-        location.href = './create_new_user.html';
-    }
-})
-
-// close login modal on escape key press
-document.addEventListener('keydown', (event)=>{
-    if (event.key === "Escape" && !loginModal.className.includes('hide-modal')) {
-        loginModal.classList.toggle('hide-modal')
-    }
-})
+function userSignUpPage(){
+    location.href = './create_new_user.html';
+}
 
 function saveLocal(user, push){
     const users = JSON.parse(localStorage.getItem("CMS_users")) || [{currentUser:''}];
@@ -112,3 +92,47 @@ function isProfileLoggedIn(href){
         + user.slice(1);
     }
 }
+
+document.addEventListener('click', (event)=>{
+    // // open/close login modal
+    // if (event.target.matches('#login-button') ||
+    //     event.target.matches('.close-icon')) {
+    //     loginModal.classList.toggle('hide-modal');
+    // }
+
+    // check user login
+    if (event.target.matches('#sign-in-button') && event.target.textContent === 'Log in'){
+        checkUserLogin();
+    }
+
+    // add new user
+    if (event.target.matches('#sign-in-button') && event.target.textContent === 'Sign up'){
+        addNewUser();
+    }
+
+    //logout
+    if (event.target.matches('#log-out')){
+        logOut();
+    }
+
+    // load user sign up
+    if (event.target.matches('#new-user-button')){
+        userSignUpPage();
+    }
+})
+
+// close login modal on escape key press
+document.addEventListener('keydown', (event)=>{
+    if (event.key === "Enter"){
+        if (location.href.includes("login.html")){
+            checkUserLogin();
+        }
+        if (location.href.includes("create_new_user.html")){
+            addNewUser();
+        }
+    }
+
+    // if (event.key === "Escape" && !loginModal.className.includes('hide-modal')) {
+    //     loginModal.classList.toggle('hide-modal')
+    // }
+})
